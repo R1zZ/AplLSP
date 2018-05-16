a164ba864477eeef7d84536e5ffecf2ecb7cda0e
Afif Rizki Kurniawan <rizky281193@gmail.com>
2018-05-16 13:40:17 +0700

    cici

--- src/components/FormInputs/FormInputs.jsx
+++ src/components/FormInputs/FormInputs.jsx
@@ -1,29 +1,56 @@
 import React, { Component } from "react";
 
 export class FormInputs extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onSubmit(event){
+  onSubmit(event) {
     event.preventDefault();
-    this.props.onAdd(this.firstName.value, this.lastName.value);
-    this.firstName.value = '';
-    this.lastName.value = '';
+    this.props.onAdd(
+      this.nik.value,
+      this.nama.value,
+      this.temptl.value,
+      this.tgllahir.value,
+      this.alamat.value,
+      this.npwp.value,
+      this.jk.value,
+      this.keahlian.value,
+      this.statusasesor.value,
+      this.jdwlasesor.value
+    );
   }
   render() {
-    return(
+    return (
       <form onSubmit={this.onSubmit}>
-        <input 
-          placeholder="Nama Depan"
-          ref={firstName => this.firstName = firstName}
-        /><p/>
         <input
-          placeholder="Nama Belakang"
-          ref={lastName => this.lastName= lastName}
-        /><p/>
+          placeholder="NIK"
+          ref={nik => this.nik = nik}
+        />
+        <input
+          placeholder="Nama"
+          ref={nama => this.nama = nama}
+        />
+        <input
+          placeholder="tempat Lahir"
+          ref={temptl => this.temptl = temptl}
+        />
+        <input
+          type="date"
+          placeholder="tanggal Lahir"
+          ref={tgllahir => this.tgllahir = tgllahir}
+        />
+        <input
+          placeholder="alamat"
+          ref={alamat => this.alamat = alamat}
+        />
+        <input
+          placeholder="NPWPr"
+          ref={npwp => this.npwp = npwp}
+        />
+
         <button>Tambah</button>
       </form>
     )
