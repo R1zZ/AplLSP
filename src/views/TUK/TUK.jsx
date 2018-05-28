837bb9ee6797ed3e368e9ce1c37d9a5021efdf73
Afif Rizki Kurniawan <rizky281193@gmail.com>
2018-05-28 22:13:15 +0700

    pembaruan

--- src/views/TUK/TUK.jsx
+++ src/views/TUK/TUK.jsx
@@ -1,36 +1,15 @@
-import React from "react";
-// react components used to create a google map
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps";
+import React, { Component } from "react";
+import 'components/css/Body/Body.css';
 
-const CustomMap = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap
-      defaultZoom={13}
-      defaultCenter={{ lat: 40.748817, lng: -73.985428 }}
-      defaultOptions={{
-        scrollwheel: false,
-        zoomControl: true
-      }}
-    >
-      <Marker position={{ lat: 40.748817, lng: -73.985428 }} />
-    </GoogleMap>
-  ))
-);
-
-function Maps({ ...prop }) {
-  return (
-    <CustomMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `100vh` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
-  );
+class TUK extends Component {
+  render() {
+    return (
+      <div className="content">
+      <div className="bgTUK">
+      </div>
+      </div>
+    );
+  }
 }
 
-export default Maps;
+export default TUK;
